aa465914036da71c48e4f77f4ded56e594bced94
jonatan050584 <jonatan_angelo@hotmail.com>
2015-10-27 11:07:50 -0500

    va

--- www/assets/js/contacto.js
+++ www/assets/js/contacto.js
@@ -6,11 +6,20 @@ var Contacto = function(){
 		window.open("https://www.google.es/maps/place/12%C2%B005'23.3%22S+77%C2%B002'22.7%22W/@-12.089802,-77.0401842,19z/data=!3m1!4b1!4m2!3m1!1s0x0:0x0", '_system');
 	});
 	$("#contacto .web").on("tap",function(){
-		var ref = cordova.InAppBrowser.open("http://picnic.pe", "_system","location=yes");
+		var ref = cordova.InAppBrowser.open("http://www.datum.com.pe", "_system","location=yes");
+		
 	})
 	$("#contacto .tel").on("tap",function(){
-		window.plugins.CallNumber.callNumber(function(){
-		alert("ok")}, function(){alert("error")}, "+5116472316");
+
+		var ref = cordova.InAppBrowser.open("tel:+5116472316", "_system","location=yes");
+		//window.plugins.CallNumber.callNumber(function(){
+		//alert("ok")}, function(){alert("error")}, "+5112150600");
+	});
+	$("#contacto .em").on("tap",function(){
+		var ref = cordova.InAppBrowser.open("mailto:datum@datum.com", "_system","location=yes");
+		
+		//window.plugins.CallNumber.callNumber(function(){
+		//alert("ok")}, function(){alert("error")}, "+5112150600");
 	})
 
 };
